c53712ef821cf0adc42d6875fd3bfc19b749e6b2
MdAqib184 <mdaqib184@gmail.com>
2023-09-22 18:47:54 +0530

    Initial commit

---
+++ script2.js
@@ -0,0 +1,70 @@
+const apikey = "927920e37864f07ef8647ffc66e904cb";
+        const searchbox = document.querySelector(".search input");
+        const searchbtn = document.querySelector(".search button");
+        const weathericon = document.querySelector(".weather-icon");
+        const voiceicon = document.querySelector(".voice-icon")
+
+        async function checkweather(city) {
+            const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`;
+
+            const response = await fetch(apiurl);
+
+            if (response.status == 404) {
+                document.querySelector(".weather").style.display = "none";
+                document.querySelector(".error").style.display = "block";
+            } 
+            else {
+                var data = await response.json();
+                document.querySelector(".city").innerHTML = data.name;
+                document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+                document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+                document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
+
+                if (data.weather[0].main == "Clear") {
+                weathericon.src = "images/clear.png";
+                } else if (data.weather[0].main == "Clouds") {
+                weathericon.src = "images/clouds.png";
+                } else if (data.weather[0].main == "Rain") {
+                weathericon.src = "images/rain.png";
+                } else if (data.weather[0].main == "Mist") {
+                weathericon.src = "images/mist.png";
+                } else if (data.weather[0].main == "Drizzle") {
+                weathericon.src = "images/drizzle.png";
+                } else if (data.weather[0].main == "Snow") {
+                weathericon.src = "images/snow.png";
+                }
+
+                document.querySelector(".weather").style.display = "block";
+                document.querySelector(".error").style.display = "none";
+            }
+        }
+
+        searchbtn.addEventListener("click", () => {
+            checkweather(searchbox.value);
+        });
+
+        if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+            const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+
+            recognition.onstart = () => {
+            // Speech recognition started.
+            console.log('Listening...');
+            };
+
+            recognition.onresult = (event) => {
+            const result = event.results[0][0].transcript;
+            searchbox.value = result;
+            };
+
+            recognition.onerror = (event) => {
+            console.error('Speech recognition error:', event.error);
+            };
+
+            voiceicon.addEventListener('click', () => {
+            recognition.start();
+            });
+        } 
+        else {
+        alert('Speech recognition is not supported in this browser.');
+        }
+
