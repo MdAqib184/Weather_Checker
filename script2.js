3b6e349324a1ba19db8dd0e44592799b3e3950ea
Uwais Jawed <uwaisjawed8081@gmail.com>
2023-09-26 22:38:12 +0530

    Final Commit

--- script2.js
+++ script2.js
@@ -1,70 +1,70 @@
 const apikey = "927920e37864f07ef8647ffc66e904cb";
-        const searchbox = document.querySelector(".search input");
-        const searchbtn = document.querySelector(".search button");
-        const weathericon = document.querySelector(".weather-icon");
-        const voiceicon = document.querySelector(".voice-icon")
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weathericon = document.querySelector(".weather-icon");
+const voiceicon = document.querySelector(".voice-icon");
 
-        async function checkweather(city) {
-            const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`;
+async function checkweather(city) {
+  const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`;
 
-            const response = await fetch(apiurl);
+  const response = await fetch(apiurl);
 
-            if (response.status == 404) {
-                document.querySelector(".weather").style.display = "none";
-                document.querySelector(".error").style.display = "block";
-            } 
-            else {
-                var data = await response.json();
-                document.querySelector(".city").innerHTML = data.name;
-                document.querySelector(".temp").innerHTML = data.main.temp + "°c";
-                document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-                document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
+  console.log(response);
 
-                if (data.weather[0].main == "Clear") {
-                weathericon.src = "images/clear.png";
-                } else if (data.weather[0].main == "Clouds") {
-                weathericon.src = "images/clouds.png";
-                } else if (data.weather[0].main == "Rain") {
-                weathericon.src = "images/rain.png";
-                } else if (data.weather[0].main == "Mist") {
-                weathericon.src = "images/mist.png";
-                } else if (data.weather[0].main == "Drizzle") {
-                weathericon.src = "images/drizzle.png";
-                } else if (data.weather[0].main == "Snow") {
-                weathericon.src = "images/snow.png";
-                }
+  if (response.status == 404) {
+    document.querySelector(".weather").style.display = "none";
+    document.querySelector(".error").style.display = "block";
+  } else {
+    const data = await response.json();
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
 
-                document.querySelector(".weather").style.display = "block";
-                document.querySelector(".error").style.display = "none";
-            }
-        }
+    if (data.weather[0].main == "Clear") {
+      weathericon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Clouds") {
+      weathericon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Rain") {
+      weathericon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Mist") {
+      weathericon.src = "images/mist.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weathericon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Snow") {
+      weathericon.src = "images/snow.png";
+    }
 
-        searchbtn.addEventListener("click", () => {
-            checkweather(searchbox.value);
-        });
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
 
-        if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
-            const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+searchbtn.addEventListener("click", () => {
+  checkweather(searchbox.value);
+});
 
-            recognition.onstart = () => {
-            // Speech recognition started.
-            console.log('Listening...');
-            };
+if ("SpeechRecognition" in window || "webkitSpeechRecognition" in window) {
+  const recognition = new (window.SpeechRecognition ||
+    window.webkitSpeechRecognition)();
 
-            recognition.onresult = (event) => {
-            const result = event.results[0][0].transcript;
-            searchbox.value = result;
-            };
+  recognition.onstart = () => {
+    // Speech recognition started.
+    console.log("Listening...");
+  };
 
-            recognition.onerror = (event) => {
-            console.error('Speech recognition error:', event.error);
-            };
+  recognition.onresult = (event) => {
+    const result = event.results[0][0].transcript;
+    searchbox.value = result;
+  };
 
-            voiceicon.addEventListener('click', () => {
-            recognition.start();
-            });
-        } 
-        else {
-        alert('Speech recognition is not supported in this browser.');
-        }
+  recognition.onerror = (event) => {
+    console.error("Speech recognition error:", event.error);
+  };
 
+  voiceicon.addEventListener("click", () => {
+    recognition.start();
+  });
+} else {
+  alert("Speech recognition is not supported in this browser.");
+}
